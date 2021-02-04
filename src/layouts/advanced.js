fa08ace9cfc24e9b512cf53aec6b549b8ffcb1ec
Stackbit Code Editor <support@stackbit.com>
2021-02-04 22:08:24 +0000

    advanced.js: changed

--- src/layouts/advanced.js
+++ src/layouts/advanced.js
@@ -3,20 +3,6 @@ import _ from 'lodash';
 
 import components, {Layout} from '../components/index';
 
-function Form() {
-  const registerUser = event => {
-    event.preventDefault() // don't redirect the page
-    // where we'll add our form logic
-  }
-
-  return (
-    <form onSubmit={registerUser}>
-      <label htmlFor="name">Name</label>
-      <input id="name" type="text" autoComplete="name" required />
-      <button type="submit">Register</button>
-    </form>
-  )
-}
 
 export default class Advanced extends React.Component {
     render() {
